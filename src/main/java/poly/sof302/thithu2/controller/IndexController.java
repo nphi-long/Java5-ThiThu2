19afd2b745dad6562a713762a9bf34fd7a776849
nphi-long <longnp.email@gmail.com>
2020-06-29 00:54:36 +0700

    Done!

--- src/main/java/poly/sof302/thithu2/controller/IndexController.java
+++ src/main/java/poly/sof302/thithu2/controller/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import poly.sof302.thithu2.entities.Chitietmonhoc;
 import poly.sof302.thithu2.service.IChiTietMonHocService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -16,16 +17,33 @@ public class IndexController {
     @Autowired
     IChiTietMonHocService ctmhService;
 
-    @GetMapping(value = {"/", "" ,"/index"})
+    @GetMapping(value = {"/", "", "/index"})
     public String returnIndexPage(ModelMap model) {
         List<Chitietmonhoc> ctmhList = ctmhService.findAll();
         model.addAttribute("ctmhList", ctmhList);
         model.addAttribute("ctmh", new Chitietmonhoc());
+
+        // Danh sách trên 5
+        double diemCaoNhat = 0;
+        Chitietmonhoc ctmhCaoNhat = null;
+        List<Chitietmonhoc> ctmhListTren5 = new ArrayList<>();
+        for (Chitietmonhoc ctmh : ctmhList) {
+            double diemTB = (ctmh.getQuiz1() + ctmh.getQuiz2() + ctmh.getQuiz3()) / 3;
+            if (diemTB > 5) {
+                ctmhListTren5.add(ctmh);
+                if (diemTB > diemCaoNhat) {
+                    diemCaoNhat = diemTB;
+                    ctmhCaoNhat = ctmh;
+                }
+            }
+        }
+        model.addAttribute("svDiemCaoNhat", ctmhCaoNhat);
+        model.addAttribute("ctmhListTren5", ctmhListTren5);
         return "index";
     }
 
     @PostMapping("/add")
-    public String addMonHoc(@ModelAttribute("ctmh") Chitietmonhoc ctmh){
+    public String addMonHoc(@ModelAttribute("ctmh") Chitietmonhoc ctmh) {
         ctmhService.saveOrUpdate(ctmh);
         return "redirect:/index";
     }
